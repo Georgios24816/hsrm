1ff007fbd0b5294b1e76e0d488ee561d9efc3a50
gmark001 <gmark001@ad.local.cs.hs-rm.de>
2021-01-04 12:28:40 +0100

    update

--- 3/hardwaresprache/5/datapath.vhd
+++ 3/hardwaresprache/5/datapath.vhd
@@ -15,6 +15,45 @@ library work;
 	use work.all;
 	use work.hadescomponents.all;
 	
+entity mux2_1 is
+	generic (WIDTH: integer := 32);
+    port ( sel : in  std_logic;
+           a   : in  std_logic_vector(WIDTH - 1 downto 0);
+           b   : in  std_logic_vector(WIDTH - 1 downto 0);
+		   
+           res : out std_logic_vector(WIDTH - 1 downto 0);
+		 );
+end mux2_1;
+
+architecture Behavioral of mux2_1 is
+begin
+    res <= b when (sel = '1') else a;
+end Behavioral;
+	
+entity FlipFlop is
+	generic (WIDTH: integer := 8);
+	
+	port(
+		clk 	: in 	std_logic;  
+		reset	: in 	std_logic;  
+		d 		: in  	std_logic_vector(WIDTH - 1 downto 0);    
+		
+		q 		: out 	std_logic_vector(WIDTH - 1 downto 0)
+	);
+end FlipFlop;
+
+architecture Behavioral of FlipFlop is  
+begin  
+	PROCESS(clk, reset)
+	BEGIN 
+		if(reset = '1') then 
+			q <= '0';	
+		elsif(rising_edge(Clk)) then
+			q <= d; 
+		end if;      
+	END PROCESS;
+end Behavioral;
+	
 entity datapath is
 	port (
 		-- common
@@ -53,9 +92,147 @@ entity datapath is
 	);
 end datapath;
 
-architecture rtl of datapath is			
+architecture rtl of datapath is
+	signal aluResult 	: std_logic_vector(31 downto 0);
+	--signal aluZero 		: std_logic;
+	--signal aluOv		: std_logic;
+	
+	signal imuxRes		: std_logic;
+	signal pcmuxRes		: std_logic_vector(31 downto 0);
+	
+	signal opregRes		: std_logic_vector( 4 downto 0);
+	signal aregRes		: std_logic_vector(31 downto 0);
+	signal bcregRes		: std_logic_vector(31 downto 0);
+	signal bregRes		: std_logic_vector(31 downto 0);
+	
+	signal xdatainRes	: std_logic_vector(31 downto 0);
+	
 begin
+	imux : mux2_1
+	generic map (WIDTH => 32);
+	port map(
+		sel => ivalid,
+		a => aop,
+		b => bop,
+		res => imuxRes
+	);
+	
+	pcmux : mux2_1
+	generic map (WIDTH => 32);
+	port map(
+		sel => jal,
+		a => aluResult,
+		b => pcinc,
+		res => pcmuxRes
+	);
+	
+	relamux : mux2_1
+	generic map (WIDTH => 32);
+	port map(
+		sel => rela,
+		a => aluResult,
+		b => aluResult,
+		res => pcnew
+	);
+	
+	dmux : mux2_1
+	generic map (WIDTH => 32);
+	port map(
+		sel => selxres,
+		a => pcmuxRes,
+		b => xdatainRes,
+		res => wop
+	);
+	
+	opreg : FlipFlop 
+	generic map (WIDTH => 5);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => opc,
+		res => opRegRes
+	);
+	
+	areg : FlipFlop 
+	generic map (WIDTH => 32);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => aop,
+		res => aregRes
+	);
+	
+	bcreg : FlipFlop 
+	generic map (WIDTH => 32);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => imuxRes,
+		res => bcregRes
+	);
+	
+	breg : FlipFlop 
+	generic map (WIDTH => 32);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => bop,
+		res => xdataout
+	);
+	
+	rreg : FlipFlop 
+	generic map (WIDTH => 32);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => bop,
+		res => aluResult
+	);
+	
+	zff : FlipFlop 
+	generic map (WIDTH => 1);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => '0',
+		q => zero
+	);
+	
+	xreg : FlipFlop 
+	generic map (WIDTH => 32);
+	port map(
+		clk => clk,
+		reset => reset,
+		d => xdatain,
+		q => xdatainRes
+	);	
+	
+	alu_ : alu
+	port map (
+		clk => clk,		
+		reset => reset,
+		
+		opcode => opc,	
+		regwrite => regwrite,
+		
+		achannel => aregRes,
+		bchannel => bcregRes,
+		
+		result => aluResult,
+		zero => zero,	
+		overflow => ov
+	);
+	
 	datapathP : PROCESS (clk, reset)
 	BEGIN
+		--if (rising_edge(clk)) then
+		--	if (opc = "01110") then --sisa Instruktion
+		--		sisalvl = (aluResult(14) & aluResult(15));
+		--	end if;
+		--	
+		--	elsif (jal = '1') then
+		--		
+		--	end if;
+	
 	END PROCESS datapathP;
 end rtl;
