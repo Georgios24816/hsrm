d44675cf297f1a76478c6d88291fbbb308838e80
Georgios24816 <georgios24816@gmail.com>
2021-05-04 16:27:01 +0200

    update

--- 4/EZV/Aufgaben/EZV2/1.c
+++ 4/EZV/Aufgaben/EZV2/1.c
@@ -1,14 +1,19 @@
 #include <wiringPi.h>
+#include <softPwm.h>
 #include <stdint.h>
 
 void mySoftPWM(uint32_t us, uint8_t dutyCycle, int pin)
 {
-    delayMicroseconds(us);
+    softPwmWrite
 }
 
 int main()
 {
     int pin = 18;
+    
+    wiringPiSetup();
+    softPwmCreate(pin, 0, 100); //wom range 0(off) - 100(on)
+
 
     while (1)
     {
