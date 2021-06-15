098a62043eac2d32659694bb72ea99827d85ff1b
Georgios24816 <georgios@gmail.com>
2021-06-15 10:59:31 +0200

    update

--- 4/EZV/Aufgaben/EZV5/main.c
+++ 4/EZV/Aufgaben/EZV5/main.c
@@ -4,13 +4,38 @@
 #include <unistd.h>
 #include <wiringPi.h>
 
+#define M_NUMPERIODS 20
+#define M_CYCLETIME 1000000
+
 volatile size_t TachoPulses = 0;
-static unsigned long Target;
-static uint8_t DutyCycle;
+unsigned long Target;
+uint8_t DutyCycle;
+size_t Rpm;
+
+unsigned int t1 = 0, t2 = 0;
+unsigned int periodSum = 0;
 
 void ISR(void)
 {
+    if (digitalRead(3) != HIGH)
+    {
+        return;
+    }
+
+    t2 = micros();
+    unsigned int period = t2 - t1;
+
     TachoPulses++;
+    t1 = t2;
+    TachoPulses++;
+
+    periodSum += period;
+
+    if (TachoPulses > M_NUMPERIODS)
+    {
+        Rpm = (30000000 * TachoPulses) / periodSum;
+        periodSum = tacho = 0;
+    }
 }
 
 int main(int argc, char** argv)
@@ -46,6 +71,8 @@ int main(int argc, char** argv)
     }
 
     wiringPiSetupGpio();
+    t1 = micros();
+
     wiringPiISR(3, INT_EDGE_RISING, ISR);
     pinMode(18, PWM_OUTPUT);
 
@@ -59,11 +86,8 @@ int main(int argc, char** argv)
 
     while (1)
     {
-        usleep(100000);
-
-        printf("\rRPM = %d", (int)(TachoPulses / 30));
-        TachoPulses = 0;
-
+        usleep(M_CYCLETIME);
+        printf("\rRPM = %d", (int)Rpm);
         fflush(stdout);
     }
 
