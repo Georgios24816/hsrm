564df7c40d97749e192eb11568877f88bd9e27cd
Georgios24816 <georgios24816@gmail.com>
2022-05-12 07:48:02 +0200

    update

---
+++ 6/VHDL/chacha_VHDL/workspace/build/secworks_crypto_chacha_0/tb_chacha-modelsim/edalize_build_rtl.tcl
@@ -0,0 +1,6 @@
+vlib work
+vlog -quiet -work work ../src/secworks_crypto_chacha_0/src/rtl/chacha_qr.v
+vlog -quiet -work work ../src/secworks_crypto_chacha_0/src/rtl/chacha_core.v
+vlog -quiet -work work ../src/secworks_crypto_chacha_0/src/rtl/chacha.v
+vlog -quiet -work work ../src/secworks_crypto_chacha_0/src/tb/tb_chacha_core.v
+vlog -quiet -work work ../src/secworks_crypto_chacha_0/src/tb/tb_chacha.v
