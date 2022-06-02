58814e261326a33e565741ff950d5a0a63f66992
Georgios24816 <georgios24816@gmail.com>
2022-06-02 20:55:15 +0200

    update

--- git.sh
+++ git.sh
@@ -6,7 +6,8 @@ spawn ./gitHelper.sh
 expect "Username for 'https://github.com': "
 send -- "Georgios24816\r"
 expect "Password for 'https://Georgios24816@github.com':"
-send -- "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\r"
+send -- "ghp_sXejLA7iIQWv1jv"
+send -- "ytBciEKb5WpRB5y2iAyrO\r"
 
 
 expect eof
